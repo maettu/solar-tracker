ab15b1cddd7a49c452fb21aa0a9d73361d31b97d
Matthias Bloch <matthias.bloch@puffin.ch>
2016-01-24 18:43:09 +0100

    replace long term graphs with graphs that rather display sums

--- mojo/solar-tracker.pl
+++ mojo/solar-tracker.pl
@@ -2,6 +2,8 @@ use Mojolicious::Lite;
 use Mojo::JSON qw (decode_json);
 use DateTime;
 use RRDs;
+use lib "../Image-BoxModel/lib"; # dev version of Image::BoxModel
+use Image::BoxModel::Chart;
 
 # write to wherever specified
 sub output ($){
@@ -39,7 +41,7 @@ get '/' => sub {
     my $c = shift;
 
     # render images
-    for my $since (1, 5, 30, 365){
+    for my $since (1, 7){
         RRDs::graph(
             "-N", "--start", "end-${since}d",
             "-w", 1000, "-h", 200,
@@ -49,10 +51,73 @@ get '/' => sub {
         );
     }
 
+    # render days totals (last 365 days)
+    my $dt = DateTime->now;
+
+    my @data;
+    my @dates;
+    for my $i (0 .. 365 ){
+        my $d = $dt->clone();
+        $d->subtract(days=>$i);
+        if (open my $dh, '<', "../raw_data/totals_log/".$d->ymd) {
+            my ($day, $year, $total) = split /\s+/ , <$dh>;
+            unshift @data, $day/1000;
+            unshift @dates, $d->ymd;
+        }
+    }
+
+    my $image = Image::BoxModel::Chart->new(
+        width=> 1200,
+        height=>300,
+        lib=>'GD',
+    );
+    $image->Chart(
+        dataset_00 => \@data,
+        style => 'bar',
+        values_annotations => \@dates,
+        values_annotation_skip => 30,
+        values_annotation_rotate => 0,
+    );
+    $image->Save(file=>"public/days_totals.png");
+
+
+    # render month's totals
+    undef @data;
+    undef @dates;
+    for my $m (0 .. 11) {
+        my $d = $dt->clone();
+        $d->subtract(months=>$m);
+        my $d_s = $d->ymd;
+        $d_s =~ s/-\d+$//;
+        say $d_s;
+        my $sum = 0;
+        for my $i (1 ..31){
+            if (open my $dh, '<', "../raw_data/totals_log/".$d_s."-".$i) {
+                my ($e, undef, undef) = split /\s+/ , <$dh>;
+                $sum += $e / 1000;
+            }
+        }
+        unshift @data, $sum;
+        unshift @dates, $d_s;
+    }
+
+    $image = Image::BoxModel::Chart->new(
+        width=> 1200,
+        height=>300,
+        lib=>'GD',
+    );
+    $image->Chart(
+        dataset_00 => \@data,
+        style => 'bar',
+        values_annotations => \@dates,
+        values_annotation_rotate => 0,
+    );
+    $image->Save(file=>"public/months_totals.png");
+
+
     my $result = RRDs::info('../rrd/solar.rrd');
     my $current_energy = $result->{'ds[watt].last_ds'};
 
-    my $dt = DateTime->now;
     my $file_name = "../raw_data/totals_log/".$dt->ymd;
     open my $fh, '<', $file_name;
     my @energies = split /\t/ , <$fh>;
@@ -104,9 +169,8 @@ __DATA__
 
 <h2>1 Day</h2>
 <img src="1d.png" alt="graph 1 day" >
-<h2>5 Days</h2>
-<img src="5d.png" alt="graph 5 days" >
-<h2>30 Days</h2>
-<img src="30d.png" alt="graph 30 days" >
-<h2>365 Days</h2>
-<img src="365d.png" alt="graph 365 days" >
+<h2>7 Days</h2>
+<h2>kWh per day</h2>
+<img src="days_totals.png" alt="totals last 365 days" >
+<h2>kWh per month</h2>
+<img src="months_totals.png" alt="totals last 12 months" >
